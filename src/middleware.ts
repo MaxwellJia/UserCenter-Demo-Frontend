39a9ae91bbce0102587bc235e1f3a1a26f02cc0e
MaxwellJia <591790421@qq.com>
2025-05-17 09:27:02 +1000

    Add a variable to fix a redirect bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ import { jwtVerify } from 'jose';
 const PUBLIC_PATHS = ["/login", "/register"];
 
 export async function middleware(req: NextRequest) {
+
     const { pathname } = req.nextUrl;
 
     // 放行公开页面
@@ -12,6 +13,7 @@ export async function middleware(req: NextRequest) {
     //  读取 Cookie 中的 token
     const token = req.cookies.get("token")?.value;
 
+
     if (!token) {
         // 没有 token，重定向回登录页
         return NextResponse.redirect(new URL("/login", req.url));
@@ -19,14 +21,16 @@ export async function middleware(req: NextRequest) {
 
     try {
         // 检查 token 是否有效 & 是否过期
+        /** JWT_SECRET在.env.local中， 不要上传到git， 请gitignore，前后端请保持一致**/
         await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET));
         return NextResponse.next(); // 放行
     } catch (err) {
+        console.log(123456789);
         // token 无效或已过期
         return NextResponse.redirect(new URL("/login", req.url));
     }
 }
 
 export const config = {
-    matcher: ["/dashboard","/dashboard/:path*"], // 仅匹配这些路径, 在http://localhost:3000/dashboard/下所有路径都会查看token是否过期
+    matcher: ["/dashboard/:path*"], // 仅匹配这些路径, 在http://localhost:3000/dashboard/下所有路径都会查看token是否过期
 };
