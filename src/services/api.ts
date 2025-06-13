79d63d860346bd43b5850910d37c61c7f85ed300
MaxwellJia <591790421@qq.com>
2025-06-13 20:21:14 +1000

    Add some features:
    reupload

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE || process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5134/api',
     headers: {
+
         'Content-Type': 'application/json',
     },
 });
