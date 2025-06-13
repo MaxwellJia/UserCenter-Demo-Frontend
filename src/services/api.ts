b697654a132d5a60b3bc04e8773904fee8a5d6c7
MaxwellJia <591790421@qq.com>
2025-06-13 20:38:37 +1000

    Add some features:

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE || process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5134/api',
+    baseURL: process.env.NEXT_PUBLIC_API_BASE || process.env.NEXT_PUBLIC_API_BASE_URL || "https://usercenter-demo-bsc9fyazcubtgka0.australiaeast-01.azurewebsites.net/api" || 'http://localhost:5134/api',
+
     headers: {
 
         'Content-Type': 'application/json',
