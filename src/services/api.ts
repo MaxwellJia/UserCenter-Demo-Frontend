680d8d7a7392cf64c3bce916070c67d4013da2e3
MaxwellJia <591790421@qq.com>
2025-06-14 15:49:58 +1000

    Add some features:
    Update api to recognize development or production

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE || process.env.NEXT_PUBLIC_API_BASE_URL || "https://usercenter-demo-bsc9fyazcubtgka0.australiaeast-01.azurewebsites.net/api" || 'http://localhost:5134/api',
+    baseURL: process.env.NEXT_PUBLIC_API_BASE ||
+        process.env.NEXT_PUBLIC_API_BASE_URL ||
+        (process.env.NODE_ENV === 'development'
+            ? 'http://localhost:5134/api'
+            : 'https://usercenter-demo-bsc9fyazcubtgka0.australiaeast-01.azurewebsites.net/api'),
 
     headers: {
 
